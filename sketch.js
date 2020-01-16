e1f9183612ca5d99533948e950a0cba41a3c1033
Atick Faisal <atickfaisal@gmail.com>
2020-01-16 15:52:20 +0600

    fixed bg

--- sketch.js
+++ sketch.js
@@ -1,46 +1,72 @@
-let sigma = 10;
-let beta = 8.0/3.0;
-let rho = 28;
+/*
+      Author  : Atick Faisal
+      Date    : 16 Jan, 2020
+*/
+
+//------------- Window Size --------------//
+const windowWidth = 600;
+const windowHeight = 400;
+//------------- Lorenz Params ------------//
+const sigma = 10;
+const beta = 8.0/3.0;
+const rho = 28;
+var dt = 0.001;
+//----------------------------------------//
 let x = [];
 let y = [];
 let z = [];
-let cols = [];
+//----------------------------------------//
+let lineColors = [];
 let points = [];
-let dt = 0.001;
-let num = 500;
+//--------- Number of particles ----------//
+const numParticles = 500;
+//----------------------------------------//
 
+////////////////////////////////////////////////////////////////////////////////
 function preload() {
-  img = loadImage('assets/sketch_bg.png');
+  backgroundImage = loadImage('assets/sketch_bg.png');
 }
-
-function setup() {
-  var mCanvas = createCanvas(600, 400);
-  mCanvas.parent("canvas_id");
-  for(let i = 0; i < num; i++) {
+////////////////////////////////////////////////////////////////////////////////
+function __randomize() {
+  for(let i = 0; i < numParticles; i++) {
     x[i] = random(-100, 100);
     y[i] = random(-10, 10);
     z[i] = random(-100, 100);
-    cols[i] = color(random(30, 225), random(30, 225), random(3, 225), 255);
+    lineColors[i] = color(random(30, 225), random(30, 225), random(3, 225), 255);
   }
+  dt = 0.001;
 }
-
+////////////////////////////////////////////////////////////////////////////////
+function __calculate_attractor(i) {
+  let dx = sigma * (y[i] - x[i]) * dt;
+  let dy = (x[i] * (rho - z[i]) - y[i]) * dt;
+  let dz = (x[i] * y[i] - beta * z[i]) * dt;
+  x[i] = x[i] + dx;
+  y[i] = y[i] + dy;
+  z[i] = z[i] + dz;
+}
+////////////////////////////////////////////////////////////////////////////////
+function setup() {
+  var mCanvas = createCanvas(windowWidth, windowHeight);
+  mCanvas.parent("canvas_id");
+  noStroke();
+  __randomize();
+}
+////////////////////////////////////////////////////////////////////////////////
 function draw() {
   translate(width/2, 0);
   background(255);
-  image(img, -width/2, 0, width, height);
-  noStroke();
-  ////////////////////////////////////////
-  for(let i = 0; i < num; i++) {
-    let dx = sigma * (y[i] - x[i]) * dt;
-    let dy = (x[i] * (rho - z[i]) - y[i]) * dt;
-    let dz = (x[i] * y[i] - beta * z[i]) * dt;
-    x[i] = x[i] + dx;
-    y[i] = y[i] + dy;
-    z[i] = z[i] + dz;
-    fill(cols[i]);
+  image(backgroundImage, -width/2, 0, width, height);
+  for(let i = 0; i < numParticles; i++) {
+    __calculate_attractor(i);
+    fill(lineColors[i]);
     ellipse(x[i] * 13, z[i] * 7, 3);
   }
   if(dt < 0.01) {
     dt += 0.00001;
   }
+}
+////////////////////////////////////////////////////////////////////////////////
+function mouseClicked() {
+  __randomize();
 }
