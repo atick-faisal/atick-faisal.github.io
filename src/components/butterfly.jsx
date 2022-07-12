de3ac170e7f7a7961ab4f81f74d44128306c0bfc
Atick Faisal <atickfaisal@gmail.com>
2022-07-12 19:51:48 +0300

    Finish lorentz attractor

--- src/components/butterfly.jsx
+++ src/components/butterfly.jsx
@@ -1,76 +1,81 @@
 import React from "react";
 import Sketch from "react-p5";
 
-//------------- Window Size --------------//
 const windowWidth = 600;
 const windowHeight = 400;
-//------------- Lorenz Params ------------//
-const sigma = 10;
-const beta = 8.0 / 3.0;
-const rho = 28;
-var dt = 0.001;
-//----------------------------------------//
-let x = [];
-let y = [];
-let z = [];
-//----------------------------------------//
-let lineColors = [];
-// let points = [];
-//--------- Number of particles ----------//
-const numParticles = 500;
-//----------------------------------------//
-// var backgroundImage = null;
 
-export default function Butterfly(props) {
-    ////////////////////////////////////////////////////////////////////////////////
-    const preload = (p5) => {
-        // p5.backgroundImage = p5.loadImage("../assets/sketch_bg.png");
-    };
+let particles = [];
+const nParticles = 300;
 
+class LorentzParticle {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        this.sigma = 10;
+        this.beta = 8.0 / 3.0;
+        this.rho = 28;
+        this.dt = 0.001;
+    }
+
+    randInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    randomize() {
+        this.x = this.randInt(-100, 100);
+        this.y = this.randInt(-10, 10);
+        this.z = this.randInt(-100, 100);
+        this.dt = 0.001;
+    }
+
+    update() {
+        this.x += this.sigma * (this.y - this.x) * this.dt;
+        this.y += (this.x * (this.rho - this.z) - this.y) * this.dt;
+        this.z += (this.x * this.y - this.beta * this.z) * this.dt;
+        if (this.dt < 0.01) this.dt += 0.00001;
+    }
+
+    copy() {
+        return new LorentzParticle(this.x, this.y, this.z);
+    }
+}
+
+export default function Butterfly() {
     const setup = (p5, canvasParentRef) => {
         var mCanvas = p5.createCanvas(windowWidth, windowHeight);
         mCanvas.parent(canvasParentRef);
-        p5.noStroke();
-        __randomize(p5);
+        p5.stroke("#E65100");
+        initialize();
     };
 
     const draw = (p5) => {
         p5.translate(p5.width / 2, 0);
-        p5.background(255);
-        // p5.image(p5.backgroundImage, -p5.width/2, 0, p5.width, p5.height);
-        for (let i = 0; i < numParticles; i++) {
-            __calculate_attractor(i);
-            p5.fill(lineColors[i]);
-            p5.ellipse(x[i] * 13, p5.height - z[i] * 7, 3);
-        }
-        if (dt < 0.01) {
-            dt += 0.000005;
-        }
-    };
-
-    const __randomize = (p5) => {
-        for (let i = 0; i < numParticles; i++) {
-            x[i] = p5.random(-100, 100);
-            y[i] = p5.random(-10, 10);
-            z[i] = p5.random(-100, 100);
-            lineColors[i] = p5.color(
-                p5.random(30, 225),
-                p5.random(30, 225),
-                p5.random(3, 225),
-                255
+        p5.background(255, 30);
+        for (let i = 0; i < nParticles; i++) {
+            let oldParticle = particles[i].copy();
+            update(i);
+            let newParticle = particles[i].copy();
+            p5.line(
+                oldParticle.x * 13,
+                p5.height - oldParticle.z * 7,
+                newParticle.x * 13,
+                p5.height - newParticle.z * 7
             );
         }
-        dt = 0.001;
     };
 
-    const __calculate_attractor = (i) => {
-        let dx = sigma * (y[i] - x[i]) * dt;
-        let dy = (x[i] * (rho - z[i]) - y[i]) * dt;
-        let dz = (x[i] * y[i] - beta * z[i]) * dt;
-        x[i] = x[i] + dx;
-        y[i] = y[i] + dy;
-        z[i] = z[i] + dz;
+    const initialize = () => {
+        for (let i = 0; i < nParticles; i++) {
+            particles[i] = new LorentzParticle(0, 0, 0);
+            particles[i].randomize();
+        }
     };
 
-    return <Sketch prelod={preload} setup={setup} draw={draw} />;
+    const update = (i) => {
+        particles[i].update();
+    };
+
+    return <Sketch setup={setup} draw={draw} />;
 }
