fa4370059d0e9f759ed1e0d084db4d5f94b1a7a2
Atick Faisal <atickfaisal@gmail.com>
2025-04-05 18:13:23 +0300

    Update site title and logo for personal branding

--- vite.config.js
+++ vite.config.js
@@ -15,21 +15,32 @@ export default defineConfig({
                 "mask-icon.svg",
             ],
             manifest: {
-                name: "React.js PWA",
+                name: "Atick Faisal",
                 short_name: "PWA",
-                description: "Template for React.js PWA",
-                theme_color: "#242424",
+                description: "Atick Faisal's Personal Website",
+                theme_color: "#282c34",
                 icons: [
                     {
-                        src: "pwa-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
                     },
                     {
-                        src: "pwa-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
                     },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
                 ],
             },
         }),
