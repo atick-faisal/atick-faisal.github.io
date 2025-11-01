9804d8a609df6f56b20a4bea1f0e0be5e7cc5c0e
Atick Faisal <atickfaisal@gmail.com>
2025-11-01 20:33:54 +0300

    perf: optimize critical request chain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     integrations: [tailwind({ applyBaseStyles: false })],
+    vite: {
+        build: {
+            cssInlineLimit: 8,
+        },
+    },
 });
